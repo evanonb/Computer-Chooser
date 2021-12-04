3aab82ec3f4eec93b81166174ffbf19de705d10e
Evan Owenby <89050989+evanonb@users.noreply.github.com>
2021-12-04 18:56:17 +0000

    form confirmation

---
+++ advisors.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	//stores data to local storage and loads post page
+  $("#signUp").click(function(){
+    var hold = $("#first").val();
+    localStorage.setItem("firstname", hold);
+    hold = $("#last").val();
+    localStorage.setItem("lastname", hold);
+    hold = $("#eadd").val();
+    localStorage.setItem("email", hold);
+    var url = "advisors_confirmation.html";
+    $(location).attr('href',url);
+  });
+});
